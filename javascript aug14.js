c56b22ebd5e22b7bfc0f43ea0ffa17a139b2e312
NaveenYendluru <naveenyendluru@gmail.com>
2024-10-14 15:45:41 +0530

    save

--- javascript aug14.js
+++ javascript aug14.js
@@ -10,3 +10,24 @@ function first(str){
     first("surya");
     first("greeshmant");
     first("lower");
+
+
+
+    function vow(input){
+        input=input.toLowerCase();
+        let arr=["a","e","i","o","u"];
+        for(let i=0;i<input.length;i++){
+        for(let j=0;j<arr.length;j++){
+        
+        if(input[i]==arr[j]){
+        input=input.replace(input[i],"")
+        }
+        
+        }
+        
+        }
+        console.log(input);
+        }
+        vow("hellO");
+        vow("WOrld");
+        vow("NaVeen");
